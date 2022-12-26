59edd66151fe131339e1449f5c3d3fcf71144e4b
Mahmoud ben Romdhane <m.benromdhane@arsela.co>
2022-12-26 03:07:41 +0100

    zfzef

--- microservices-nestjs-graphql-micro-users/src/users/dto/create-user.input.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
-
-@InputType()
-export class CreateUserInput {
-  @Field(() => String, { description: 'name of the user' })
-  name: string;
-  @Field(() => String, { description: 'role of the user' })
-  role: string;
-}
